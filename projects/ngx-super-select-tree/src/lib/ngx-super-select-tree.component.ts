4522c3c9a601e434aad82647672b7adc2d1cdf9e
Hesam Kashefi <hesamkashefi@yahoo.com>
2024-09-17 14:02:28 +0330

    added select children when parent gets selected fixed navigation

--- projects/ngx-super-select-tree/src/lib/ngx-super-select-tree.component.ts
+++ projects/ngx-super-select-tree/src/lib/ngx-super-select-tree.component.ts
@@ -61,19 +61,9 @@ export class NgxSuperSelectTreeComponent {
   }
 
   onCheckChanged(item: any) {
-    const value = item[this.valuePropertyName];
+    this.checkChanged(item);
 
-    const selectedValues = this.selectedValues;
-
-    const index = selectedValues.findIndex(x => x === value);
-    if (index >= 0) {
-      selectedValues.splice(index, 1);
-    }
-    else {
-      selectedValues.push(value);
-    }
-
-    this.selectedValuesChanged.emit(selectedValues);
+    this.selectedValuesChanged.emit(this.selectedValues);
   }
 
   isChecked(item: any) {
@@ -84,4 +74,47 @@ export class NgxSuperSelectTreeComponent {
     return index >= 0;
   }
 
+  navigationStack: any[] = [];
+
+  openItem(item: any) {
+    if (this.currentOpenParentItem) {
+      this.navigationStack.push(this.currentOpenParentItem)
+    }
+    this.currentOpenParentItem = item;
+  }
+
+  goBack() {
+    if (this.currentOpenParentItem !== undefined) [
+      this.currentOpenParentItem = this.navigationStack.pop()
+    ]
+  }
+
+  private selectAllDescendents(item: any) {
+    const currentParentId = item ? item[this.parentIdReferenceKeyPropertyName || this.valuePropertyName] : undefined;
+
+    const data = this.dataSource.filter(x => {
+      return x[this.parentIdPropertyName] === currentParentId
+    });
+
+    data.forEach(child => {
+      this.checkChanged(child, false);
+    });
+  }
+
+  private checkChanged(item: any, removeIfAlreadyAdded: boolean = true) {
+    const value = item[this.valuePropertyName];
+
+    const selectedValues = this.selectedValues;
+
+    const index = selectedValues.findIndex(x => x === value);
+    if (index >= 0) {
+      if (removeIfAlreadyAdded) {
+        selectedValues.splice(index, 1);
+      }
+    }
+    else {
+      selectedValues.push(value);
+      this.selectAllDescendents(item);
+    }
+  }
 }
